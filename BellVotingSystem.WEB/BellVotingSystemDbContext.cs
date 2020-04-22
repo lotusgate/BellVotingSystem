cd47e9147ab64c206c08765de47981dcad148a62
misomisev <35681808+misomisev@users.noreply.github.com>
2020-04-22 11:18:19 +0300

    Added identification and Entry/User controllers.

--- BellVotingSystem.WEB/BellVotingSystemDbContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BellVotingSystem.WEB
-{
-    internal class BellVotingSystemDbContext
-    {
-    }
-}
---
+++ BellVotingSystem.WEB/Controllers/EntryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BellVotingSystem.WEB.Controllers
+{
+    public class EntryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
