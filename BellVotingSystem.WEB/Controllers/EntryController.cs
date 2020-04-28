7a9513b6e15beb10cb05bdf3ba4652b8396f762b
Martin Jivkov <martin.jivkov5@gmail.com>
2020-04-28 15:12:39 +0300

    Added read method for Entry and User Controller

--- BellVotingSystem.WEB/Controllers/EntryController.cs
+++ BellVotingSystem.WEB/Controllers/EntryController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BellVotingSystem.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using BellVotingSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using BellVotingSystem.WEB.Models;
+using System.Threading.Tasks;
 
 namespace BellVotingSystem.WEB.Controllers
 {
     public class EntryController : Controller
     {
+        private readonly VotingSystemDbContext context;
+
+        public EntryController(VotingSystemDbContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [ActionName("Read")]
+        public async Task<IActionResult> Read(string id)
+        {
+            Entry entry = await context.Entries.SingleOrDefaultAsync(e => e.Id == id);
+
+            EntryViewModel entryViewModel = new EntryViewModel()
+            {
+                Song = entry.Song,
+                Artist = entry.Artist,
+                VoteCount = entry.VoteCount,
+                IsBlacklisted = entry.IsBlacklisted,
+                ChosenOn = entry.ChosenOn,
+            };
+
+            return View("", entryViewModel);
         }
     }
 }
--- BellVotingSystem.WEB/Controllers/UserController.cs
+++ BellVotingSystem.WEB/Controllers/UserController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BellVotingSystem.Data;
+using BellVotingSystem.Data.Models;
+using BellVotingSystem.WEB.Models.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace BellVotingSystem.WEB.Controllers
 {
     public class UserController : Controller
     {
+        private readonly VotingSystemDbContext context;
+
+        public UserController(VotingSystemDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+
+             
+        }
+
+        [HttpGet]
+        [ActionName("Read")]
+        public async Task<IActionResult> Read(string id)
+        {
+            User user = await context.Users.SingleOrDefaultAsync(u => u.Id == id);
+
+            UserViewModel userViewModel = new UserViewModel()
+            {
+                Username = user.UserName,
+                Email = user.Email,
+                HasVoted = user.HasVoted
+            };
+
+            return View("", userViewModel);
         }
     }
 }
