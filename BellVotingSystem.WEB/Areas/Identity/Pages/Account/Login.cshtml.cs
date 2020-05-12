ba56356fd7a1f48c65013d6a178ebb671a9187ef
LotusGate <35681808+LotusGate@users.noreply.github.com>
2020-05-12 22:27:16 +0300

    Removed unnecessary directories.

--- BellVotingSystem.WEB/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BellVotingSystem.WEB/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,13 +1,10 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using BellVotingSystem.Data.Models;
 
 namespace BellVotingSystem.WEB.Areas.Identity.Pages.Account
