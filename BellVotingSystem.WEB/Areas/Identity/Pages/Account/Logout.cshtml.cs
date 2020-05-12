dd82cfe3c4a6a30f3df68d07a42b93e8df206fe4
LotusGate <35681808+LotusGate@users.noreply.github.com>
2020-05-12 22:25:59 +0300

    Fixed an issue that prevented logging out.

--- BellVotingSystem.WEB/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ BellVotingSystem.WEB/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -17,22 +17,11 @@ namespace BellVotingSystem.WEB.Areas.Identity.Pages.Account
             _signInManager = signInManager;
         }
 
-        public void OnGet()
-        {
-        }
-
-        public async Task<IActionResult> OnPost(string returnUrl = null)
+        public async Task<IActionResult> OnGet()
         {
             await _signInManager.SignOutAsync();
-            
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                return RedirectToPage();
-            }
+
+            return RedirectToPage("Login");
         }
     }
 }
